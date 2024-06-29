e327287dd0623b54d9f42df36c0a7d2fdda5a05c
Hasyiqin7 <d20221102530@siswa.upsi.edu.my>
2024-06-29 16:51:16 +0800

    Update and rename loginuser.php to login-user.php

---
+++ admin/login-user.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+   <html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+      <!--=============== REMIXICONS ===============-->
+      <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+      <link rel="stylesheet" href="css/style.css" /> 
+      <title>login User</title>
+   </head>
+   <body>
+      <div class="login">
+         <img src="img/pic1register.jpg" alt="login image" class="login__img">
+
+         <form action="" class="login__form">
+            <h1 class="login__title">Login</h1>
+
+            <div class="login__content">
+               <div class="login__box">
+                  <i class="ri-user-3-line login__icon"></i>
+
+                  <div class="login__box-input">
+                     <input type="username" required class="login__input" id="login-username" placeholder=" ">
+                     <label style="color: var(--white-color)"for="login-username" class="login__label">Username</label>
+                  </div>
+               </div>
+
+               <div class="login__box">
+                  <i class="ri-lock-2-line login__icon"></i>
+
+                  <div class="login__box-input">
+                     <input type="password" required class="login__input" id="login-password" placeholder=" ">
+                     <label style="color: var(--white-color)" for="login-password" class="login__label">Password</label>
+                     <i class="ri-eye-off-line login__eye" id="login-eye"></i>
+                  </div>
+               </div>
+            </div>
+
+            <div class="login__check">
+               <p class="login__register">
+               Don't have an account? <a href="register-user.php">Register</a>
+            </p>
+            </div>
+
+            <button type="submit" class="login__button">Login</button>
+
+             <p class="login__register">
+               Are you admin ? <a href="login-admin.php">Login</a>
+            </p>
+         </form>
+      </div>
+
+   </body>
+</html>
