95b1d2cd542662dc656d82cb91929bb723d72488
dylaascreate <dylaascreate@gmail.com>
2024-07-07 15:51:16 +0800

    update

--- header.php
+++ header.php
@@ -42,7 +42,6 @@
 
 			<div class="collapse navbar-collapse" id="ftco-nav">
 				<ul class="navbar-nav ml-auto">
-					<li class="nav-item <?php echo isActive('index.php'); ?>"><a href="index.php" class="nav-link">Home</a></li>
 					<li class="nav-item <?php echo isActive('about.php'); ?>"><a href="about.php" class="nav-link">About</a></li>
 					<li class="nav-item <?php echo isActive('destination.php'); ?>"><a href="destination.php" class="nav-link">Destination</a></li>
 					<li class="nav-item <?php echo isActive('hotel.php'); ?>"><a href="hotel.php" class="nav-link">Hotel</a></li>
