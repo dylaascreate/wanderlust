0ca6bedc6a1a77085a402523b46bd80eb56de043
Hasyiqin7 <d20221102530@siswa.upsi.edu.my>
2024-07-09 11:49:53 +0800

    Update logout-user.php

--- user/logout-user.php
+++ user/logout-user.php
@@ -11,5 +11,5 @@ echo"<link rel='stylesheet' type='text/css' href='css/stylesheet.css'>";
 echo"<br>";
 echo"<p style='text-align:center'>you have successfully logged out. Thank You</p>";
 echo"<p style='text-align:center;'>Log Out ...</p>";
-echo"<meta http-equiv=\"refresh\" content=\"2; URL=index.php\">";
+echo"<meta http-equiv=\"refresh\" content=\"2; URL=../index.php\">";
 ?>
